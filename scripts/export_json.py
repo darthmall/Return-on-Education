94c7179f6d9f8789c2861657acd0d55e7043e685
darthmall <wallace.sheehan@gmail.com>
2012-10-22 16:35:57 -0600

    Export JSON to a single variable and annotate with level of attainment.

--- scripts/export_json.py
+++ scripts/export_json.py
@@ -5,20 +5,35 @@ import json
 import sqlite3
 
 
-def main(args):
-    db = sqlite3.connect(args.db)
-    c = db.cursor()
-
+def readData(c, table):
     rows = c.execute('''SELECT country, gender, cost_ratio, benefit_ratio,
         private_costs, private_benefits, public_costs, public_benefits
-        FROM {}'''.format(args.table))
+        FROM {}'''.format(table))
 
     obj = [dict(itertools.izip(('country', 'gender', 'cost_ratio',
         'benefit_ratio', 'private_costs', 'private_benefits', 'public_costs',
         'public_benefits'),
         row)) for row in rows]
 
-    print '{} = {};'.format(args.varname, json.dumps(obj, indent=2))
+    return obj
+
+
+def main(args):
+    db = sqlite3.connect(args.db)
+    c = db.cursor()
+
+    post_secondary = readData(c, 'post_secondary_overview')
+    for o in post_secondary:
+        o['attainment'] = 'post-secondary'
+
+    tertiary = readData(c, 'tertiary_overview')
+    for o in tertiary:
+        o['attainment'] = 'tertiary'
+
+    data = filter(lambda d: bool(d['benefit_ratio']),
+        itertools.chain(post_secondary, tertiary))
+
+    print '{} = {};'.format(args.varname, json.dumps(data, indent=2))
 
 
 if __name__ == '__main__':
@@ -27,7 +42,6 @@ if __name__ == '__main__':
     parser = ArgumentParser("Export data from a SQLite database to JSON")
     parser.add_argument('db', metavar='SQLITE',
         help='Path to the SQLite database')
-    parser.add_argument('table', metavar='TABLENAME', help='Table name')
     parser.add_argument('varname', metavar='VARIABLE',
         help='The name of the global JavaScript variable to store the JSON to')
     args = parser.parse_args()
