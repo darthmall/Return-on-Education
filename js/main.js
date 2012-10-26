9c7be753e0372fd0ed6f9a7733dfd66c54143d6f
darthmall <wallace.sheehan@gmail.com>
2012-10-26 11:31:48 -0600

    Create a (boring) scatterplot of costs vs. benefits
    Created a scatterplot of the private costs vs. benefits using some of
    Tufte's tips for using axes to convey more interesting data. This
    scatterplot is still boring, though. There are a couple of interesting
    outliers, but not not much.

--- js/main.js
+++ js/main.js
@@ -1,151 +1,76 @@
 (function() {
-	var WIDTH = 960, HEIGHT = 900, MARGIN = 75, RADIUS_EXTENT = [5, 40],
-		svg = d3.select('#benefits').attr('width', WIDTH).attr('height', HEIGHT),
-		data = [],
-		force = d3.layout.force().links([]).gravity(0).friction(0.7).charge(0).size([WIDTH, HEIGHT]),
-		x = d3.scale.linear().range([MARGIN + RADIUS_EXTENT[1],
-			WIDTH - MARGIN - RADIUS_EXTENT[1]]),
-		y = d3.scale.log().range([HEIGHT - RADIUS_EXTENT[1],
-			RADIUS_EXTENT[1]]),
-		a = d3.scale.linear().range([Math.PI * RADIUS_EXTENT[0] * RADIUS_EXTENT[0],
-			Math.PI * RADIUS_EXTENT[1] * RADIUS_EXTENT[1]]);
+	// Set up the dimensions
+	var margin = { top: 20, right: 60, bottom: 60, left: 100 },
+		width = benefits.clientWidth - margin.left - margin.right,
+		height = benefits.clientHeight - margin.top - margin.bottom;
 
-		force.on('tick', onTick);
+	// Groups for the different charts
+	var svg = d3.select('#benefits')
+		.append('g')
+		.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+		
 
-		d3.selectAll('select').on('change', invalidate);
+	var data = [];
 
-		invalidate();
+	// Layout for the balloon svg
+	var x = d3.scale.linear().range([0, width]),
+		y = d3.scale.linear().range([height, 0]);
 
-		function invalidate(e) {
-			data = INCENTIVES.filter(function (el) {
-				return ((gender.value === 'both' || el.gender === gender.value) &&
-					(attainment.value === 'both' || el.attainment === attainment.value));
-			});
+	var xAxis = d3.svg.axis().scale(x).orient('bottom'),
+		yAxis = d3.svg.axis().scale(y).orient('left');
 
-			x.domain(d3.extent(data, getAbsoluteValue));
-			y.domain(d3.extent(data, getValue));
-			a.domain(d3.extent(data, function (d) { return Math.abs(d.private_costs); }));
+	d3.selectAll('select').on('change', invalidate);
 
-			data.forEach(function (el) {
-				el.radius = r(el);
+	invalidate();
 
-				if (!el.y) {
-					el.y = y(getValue(el));
-				}
+	function invalidate(e) {
+		data = INCENTIVES.filter(function (el) {
+			return ((gender.value === 'both' || el.gender === gender.value) &&
+				(attainment.value === 'both' || el.attainment === attainment.value));
+		});
 
-				if (!el.x) {
-					el.x = Math.random() * (WIDTH - 2 * MARGIN) + MARGIN;
-				}
-			});
+		x.domain([0, d3.max(data, function (d) { return Math.abs(d.private_costs); })]);
+		xAxis.tickValues(data.map(function (d) { return Math.abs(d.private_costs); }));
 
-			force.stop();
-			force.nodes(data);
+		y.domain([0, d3.max(data, function (d) { return d.private_benefits; })]);
+		yAxis.tickValues(data.map(function (d) { return d.private_benefits; }));
 
-			var circles = svg.selectAll('circle')
-				.data(data, function (d) { return d.country + d.gender + d.attainment; });
+		var circles = svg.selectAll('circle')
+			.data(data, function (d) { return d.country + d.gender + d.attainment; });
 
-			circles.enter()
-				.append('circle')
-				.attr('cx', function (d) { return d.x; })
-				.attr('cy', function (d) { return d.y; })
-				.attr('class', function (d) { return d.attainment + '_' + d.gender; })
-				.transition().duration(750)
-				.attr('r', function (d) { return d.radius; });
+		circles.enter().append('circle')
+			.attr('r', 5)
+			.attr('class', function (d) { return d.attainment + '_' + d.gender; })
+			.on('mouseover', function (d) {
+				circles.filter(function (c) { return d.country !== c.country; })
+					.transition().duration(750)
+					.attr('opacity', 0.2);
+				console.log(d.country + ' ' + d.private_npv);
+			})
+			.on('mouseout', function (d) { circles.transition().duration(750).attr('opacity', 1); });
 
-			circles.transition().duration(750)
-				.attr('r', function (d) { return d.radius; });
-			circles.call(force.drag);
+		circles.transition().duration(500)
+			.attr('cx', function (d) { return x(Math.abs(d.private_costs)); })
+			.attr('cy', function (d) { return y(d.private_benefits); });
 
-			circles.exit()
-				.transition().duration(750)
-				.attr('r', 0)
-				.remove();
+		circles.exit()
+			.transition().duration(750)
+			.attr('r', 0)
+			.remove();
 
-			force.start();
-		}
+		svg.append('g')
+			.attr('class', 'x axis')
+			.attr('transform', 'translate(0,' + height + ')')
+			.call(xAxis);
 
-		function getAbsoluteValue(d) {
-			if (display.value === 'benefits') {
-				return mode.value === 'public' ? d.public_benefits : d.private_benefits;
-			}
+		svg.append('g')
+			.attr('class', 'y axis')
+			.call(yAxis);
 
-			return mode.value === 'public' ? d.public_costs : d.private_costs;
-		}
+		svg.selectAll('.x text')
+			.style('display', function (d) { return (d === x.domain()[1]) ? null : 'none'; });
 
-		function getValue(d) {
-			var v = null;
-
-			switch (mode.value) {
-				case 'private':
-					v = display.value === 'benefits' ? d.private_benefits : d.private_costs;
-					break;
-
-				case 'public':
-					v = display.value === 'benefits' ? d.public_benefits : d.public_costs;
-					break;
-
-				default:
-					v = display.value === 'benefits' ? d.benefit_ratio : d.cost_ratio;
-					break;
-			}
-
-			return v;
-		}
-
-		function getGender(d) {
-			return d.gender;
-		}
-
-		function r(d) {
-			return Math.sqrt(a(Math.abs(d.private_costs)) / Math.PI);
-		}
-
-		function onTick(e) {
-			var q = d3.geom.quadtree(data),
-				i = 0,
-				n = data.length,
-				k = 0.1 * e.alpha;
-
-			while (++i < n) {
-				q.visit(collide(e, data[i]));
-				data[i].y += (y(getValue(data[i])) - data[i].y) * k;
-				data[i].x = Math.min(Math.max(data[i].x, data[i].radius), WIDTH - data[i].radius);
-				data[i].y = Math.min(Math.max(data[i].y, data[i].radius), HEIGHT - data[i].radius);
-			}
-
-
-			svg.selectAll("circle")
-				.attr("cx", function(d) { return d.x; })
-				.attr("cy", function(d) { return d.y; });
-
-			return false;
-		}
-
-		function collide(e, node) {
-			var r = node.radius + 16,
-				nx1 = node.x - r,
-				nx2 = node.x + r,
-				ny1 = node.y - r,
-				ny2 = node.y + r;
-
-			return function(quad, x1, y1, x2, y2) {
-				if (quad.point && (quad.point !== node)) {
-					var _x = node.x - quad.point.x,
-						_y = node.y - quad.point.y,
-						l = Math.sqrt(_x * _x + _y * _y),
-						r = node.radius + quad.point.radius;
-
-					if (l < r) {
-						l = (l - r) / l * 0.5;
-						node.x -= _x *= l;
-						node.y -= _y *= l;
-
-						quad.point.x += _x;
-						quad.point.y += _y;
-					}
-				}
-
-				return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
-			};
-		}
+		svg.selectAll('.y text')
+			.style('display', function (d) { return (d === y.domain()[1]) ? null : 'none'; });
+	}
 })();
