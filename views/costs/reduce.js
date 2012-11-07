a43dc480b8cb9358a4600b6bf5912ae1c33df04a
darthmall <wallace.sheehan@gmail.com>
2012-11-07 10:11:08 -0700

    Remove all unused views and lists

--- views/costs/reduce.js
+++
@@ -1,7 +0,0 @@
-function(keys, values, rereduce) {
-  if (rereduce) {
-    return stats(values);
-  }
-
-  return values.length;
-}
--- views/npv/map.js
+++
@@ -1,6 +0,0 @@
-function(doc) {
-    if (doc.type === 'country' && !isNaN(doc['net present value'])) {
-      emit([doc.country, doc.attainment, doc.gender, doc.sector],
-        doc['net present value']);
-    }
-}
